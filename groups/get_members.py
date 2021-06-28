f73131c03f15cd6e2524ab1d0e151bcf812639f9
PatriotRossii <patriotrossii2019@mail.ru>
2021-06-28 20:06:33 +0300

    Add fields parameter

--- groups/get_members.py
+++ groups/get_members.py
@@ -1,8 +1,8 @@
 import time
 import vk
 
-def get_members(api: vk.API, group_id: int, delay: float = 0.4):
-	spy_requests = api.groups.getMembers(group_id=group_id)
+def get_members(api: vk.API, group_id: int, fields: str = "", delay: float = 0.4):
+	spy_requests = api.groups.getMembers(group_id=group_id, fields=fields)
 	count = spy_requests["count"]
 	members = set(spy_requests["items"])
 	
@@ -10,6 +10,6 @@ def get_members(api: vk.API, group_id: int, delay: float = 0.4):
 		for i in range(1, (count // 1000) + 1):
 			time.sleep(delay)
 			members.update(
-				set(api.groups.getMembers(group_id=group_id, offset=i*1000)["items"])
+				set(api.groups.getMembers(group_id=group_id, fields=fields, offset=i*1000)["items"])
 			)
 	return members
