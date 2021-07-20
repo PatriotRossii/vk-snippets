a73e7415fedf88abde3a498df0c8d3faf2df9c9e
PatriotRossii <patriotrossii2019@mail.ru>
2021-07-20 07:30:30 +0000

    Fix find members intersection script

--- groups/find_members_intersection.py
+++ groups/find_members_intersection.py
@@ -5,7 +5,7 @@ import vk
 from get_members import get_members
 
 def find_intersection(api, *group_ids):
-	members = [get_members(api, group_id) for group_id in group_ids]
+	members = [set(get_members(api, group_id)) for group_id in group_ids]
 	return set.intersection(*members)
 
 if __name__ == "__main__":
@@ -16,4 +16,4 @@ if __name__ == "__main__":
 	api = vk.API(session, v=version)
 
 	group_ids = [int(e.strip()) for e in sys.stdin.readlines()]
-	print(api, *group_ids)
+	print(find_intersection(api, *group_ids))
